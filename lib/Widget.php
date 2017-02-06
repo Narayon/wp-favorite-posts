6af63be861039660a40b097611489c5791bc3fe7
Ricardo Castelhano <ricardocastelhano@gmail.com>
2017-02-06 23:23:04 +0000

    Code Review
    Comentários inline //<-

--- lib/Widget.php
+++ lib/Widget.php
@@ -71,6 +71,7 @@ class Widget extends \WP_Widget {
 		// outputs the options form on admin
 		$title = ! empty( $instance['title'] ) ? $instance['title'] : __( 'New title', 'text_domain' );
 		?>
+//<- Coloca este pedaço todo dentro de um printf e usa as escape functions
 			<p>
 			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
 			<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
